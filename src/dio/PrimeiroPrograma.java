373a89c408a509fde09b1a61d07b00bc2ae1c2a2
augusto642 <augusto2.5@hotmail.com>
2023-03-02 00:13:06 -0300

    primeiro commit

---
+++ src/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package dio;
+
+import com.sun.tools.javac.Main;
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+/*
+        int a= 5;
+        int b= 5;
+        System.out.println("hello World " + (a+b));*/
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+    }
+}
